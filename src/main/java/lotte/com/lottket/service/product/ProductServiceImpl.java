af0434e3e9be2989dff64df5625d3e3b6d92dd5e
SSOsh <tngh147258@kumoh.ac.kr>
2022-07-18 15:33:23 +0900

    Add : User, Product, Junit

---
+++ src/main/java/lotte/com/lottket/service/product/ProductServiceImpl.java
@@ -0,0 +1,35 @@
+package lotte.com.lottket.service.product;
+
+import lotte.com.lottket.dto.ProductDto;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public void insertProduct(ProductDto dto) {
+
+    }
+
+    @Override
+    public void updateProduct(ProductDto dto) {
+
+    }
+
+    @Override
+    public void deleteProduct(ProductDto dto) {
+
+    }
+
+    @Override
+    public void selectProduct(ProductDto dto) {
+
+    }
+
+    @Override
+    public void selectProductAll() {
+
+    }
+
+    @Override
+    public void selectProductFind(String productTitle) {
+
+    }
+}
