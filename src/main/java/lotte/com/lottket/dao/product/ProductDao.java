646d44682951f81bcf853aefce9a1fdae56f8f4f
SSOsh <tngh147258@kumoh.ac.kr>
2022-07-19 10:02:28 +0900

    ADD : modify sqls and Product controller, service, dao

--- src/main/java/lotte/com/lottket/dao/product/ProductDao.java
+++ src/main/java/lotte/com/lottket/dao/product/ProductDao.java
@@ -2,11 +2,17 @@ package lotte.com.lottket.dao.product;
 
 import lotte.com.lottket.dto.ProductDto;
 
+import java.util.List;
+
 public interface ProductDao {
-    void insertProduct(ProductDto dto);
-    void updateProduct(ProductDto dto);
-    void deleteProduct(ProductDto dto);
-    void selectProduct(ProductDto dto);
-    void selectProductAll();
-    void selectProductFind(String productTitle);
+    int insertProduct(ProductDto dto);
+    int updateProduct(ProductDto dto);
+    int deleteProduct(ProductDto dto);
+    ProductDto selectProduct(ProductDto dto);
+    List<ProductDto> selectProductAll();
+    List<ProductDto> selectProductFind(String productTitle);
+    Long getId(ProductDto dto);
+    List<ProductImageDto> selectBestProduct();
+    List<ProductImageDto> selectWeeklyBestProduct();
+    List<ProductDto> selectNewProduct();
 }
--- src/main/java/lotte/com/lottket/dao/product/ProductDaoImpl.java
+++ src/main/java/lotte/com/lottket/dao/product/ProductDaoImpl.java
@@ -1,41 +1,66 @@
 package lotte.com.lottket.dao.product;
 
 import lotte.com.lottket.dto.ProductDto;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductDaoImpl implements ProductDao{
     @Autowired
-    ProductDao dao;
+    SqlSession session;
+
+    String ns = "Product.";
 
     @Override
-    public void insertProduct(ProductDto dto) {
-        dao.insertProduct(dto);
+    public int insertProduct(ProductDto dto) {
+        return session.insert(ns + "insertProduct", dto);
     }
 
     @Override
-    public void updateProduct(ProductDto dto) {
-        dao.updateProduct(dto);
+    public int updateProduct(ProductDto dto) {
+        return session.update(ns + "updateProduct", dto);
     }
 
     @Override
-    public void deleteProduct(ProductDto dto) {
-        dao.deleteProduct(dto);
+    public int deleteProduct(ProductDto dto) {
+        return session.delete(ns + "deleteProduct", dto);
     }
 
     @Override
-    public void selectProduct(ProductDto dto) {
-        dao.selectProduct(dto);
+    public ProductDto selectProduct(ProductDto dto) {
+        return session.selectOne(ns + "selectProduct", Long.toString(dto.getProductId()));
     }
 
     @Override
-    public void selectProductAll() {
-        dao.selectProductAll();
+    public List<ProductDto> selectProductAll() {
+        return session.selectList(ns + "selectProductAll");
     }
 
     @Override
-    public void selectProductFind(String productTitle) {
-        dao.selectProductFind(productTitle);
+    public List<ProductDto> selectProductFind(String productTitle) {
+        return session.selectList(ns + "selectProductFind", productTitle);
+    }
+
+    @Override
+    public Long getId(ProductDto dto) {
+        return session.selectOne(ns + "getId", dto);
+    }
+
+    @Override
+    public List<ProductImageDto> selectBestProduct() {
+        return session.selectList(ns + "selectBestProduct");
+    }
+
+    @Override
+    public List<ProductImageDto> selectWeeklyBestProduct() {
+        return session.selectList(ns + "selectBestProduct");
+    }
+
+    @Override
+    public List<ProductDto> selectNewProduct() {
+        return session.selectList(ns + "selectBestProduct");
     }
 }
